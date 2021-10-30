118619340f6fcdd5dc7d4c0fdbe8e6aa1b3d6327
Javier Ramirez <travi_javier@hotmail..com>
2021-10-30 15:45:36 -0500

    version finalizada

---
+++ src/Componentes/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <div>
+      Creado por javier Ramirez
+    </div>
+    <div>
+      <a href="https://www.instagram.com/mictlantecuhtlimx/?hl=es">Instagram</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
